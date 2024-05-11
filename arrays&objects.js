5b5a5e2eed3faa4f1dc52c828484938c608ed14e
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-05-11 09:58:37 +0300

    array & objects questions

---
+++ arrays&objects.js
@@ -0,0 +1,29 @@
+//  Define an array of user objects with a name, age and if they are employed. Hint: this is an array of objects
+
+// Define a function that destructively appends a new user to the array.
+
+// Define a function that destructively prepends a new user to the array.
+
+// Define a function that destructively removes the last user in the array.
+
+// Define a function that destructively removes the first user in the array.
+
+// Define a function that non destructively add a new user to the array.
+
+// Define an array of the first 5 prime numbers in order.
+
+// Define a function that uses the splice method to remove the third element from the numbers array.
+
+// Given an array of flowers, define a function that splits it in half and save each half to a variable eg halfOne, halfTwo
+const flowers =["roses", "orchids", "peonies", "carnations", "tulips", "daisies"]
+
+// Define an object of a student that has the following keys, name- the student's name,  subjects - an array of 5 subjects they take at school, teacher - name of their teacher, address - an object with city (a string of the city they live in), street (a string of the street name) and postalCode( an integer of 4 numeric characters)
+
+// Define a function that logs to the console their address as one string in the order postal code, street name and city
+
+// Define a function that updates the array of subjects to 7 subjects
+
+// Define a function to delete the teacher's name from the object
+
+
+
